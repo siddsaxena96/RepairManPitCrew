e8baa2ba3df5c7ef3412613352edd177cee1b1fd
Siddharth Saxena <sidd.saxena96@gmail.com>
2020-02-02 18:06:57 +0530

    +Projectile Behaviour updated

--- Assets/Scripts/ProjectileBehaviour.cs
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -6,24 +6,15 @@ public class ProjectileBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject powerUp = null;
     [SerializeField] private AudioClip oh = null;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "NANGE")
-        {
+        {            
             AudioPlayer.Instance.PlayOneShot(oh);
             //Instantiate(powerUp, col.transform.position, Quaternion.identity);
+            Debug.Log("ASD");
+            CameraShake.instance.Shake(0.1f,0.5f);
             Destroy(col.gameObject);
             Destroy(this.gameObject);
         }
