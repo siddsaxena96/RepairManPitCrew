27c122bb218cdbd0e3ee10c70342ed992504dadf
Siddharth Saxena <sidd.saxena96@gmail.com>
2020-02-02 07:10:47 +0530

    Player projectile changed to Wrench

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private PlayerController playerController = null;
+    [SerializeField] private Mover[] backgroundMovers = null;
+    [SerializeField] private ObstacleSpawner obstacleSpawner = null;
+
+    private void Awake()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {        
+        for(int i =0;i<backgroundMovers.Length;i++)
+            backgroundMovers[i].StartMoving();
+        obstacleSpawner.StartSpawning();
+        playerController.move = true;
+    }
+}
