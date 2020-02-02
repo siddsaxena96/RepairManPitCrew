408d28fde99691c3fbbd549ffba71d54da5f1d40
Siddharth Saxena <sidd.saxena96@gmail.com>
2020-02-02 07:46:50 +0530

    +GameController added +Bike chase sequence +Bike Dock sequence playable

---
+++ Assets/Scripts/BrokenCycleBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenCycleBehaviour : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb = null;
+    [SerializeField] private Vector2 stopPoint = Vector2.zero;
+    public float speed;
+
+    private void Awake()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+
+    private void Update()
+    {
+        if (rb.position.x <= 0)
+            rb.velocity = Vector2.zero;
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] private PlayerController playerController = null;
     [SerializeField] private Mover[] backgroundMovers = null;
     [SerializeField] private ObstacleSpawner obstacleSpawner = null;
+    [SerializeField] private Slider distanceSlider = null;
+    [SerializeField] private int secondsTillCycle = 15;
+    [SerializeField] private GameObject brokenCycle = null;
+    [SerializeField] private Transform brokenCycleSpawnPoint = null;
+
+    private int currentWait = 0;
+    private bool pursuitOn = true;
 
     private void Awake()
     {
@@ -14,10 +22,31 @@ public class GameController : MonoBehaviour
     }
 
     public void StartGame()
-    {        
-        for(int i =0;i<backgroundMovers.Length;i++)
+    {
+        for (int i = 0; i < backgroundMovers.Length; i++)
             backgroundMovers[i].StartMoving();
         obstacleSpawner.StartSpawning();
         playerController.move = true;
+        currentWait = secondsTillCycle;
+        StartCoroutine(ReachTillCycleCoroutine());
+    }
+
+    IEnumerator ReachTillCycleCoroutine()
+    {
+        distanceSlider.maxValue = currentWait;
+        distanceSlider.value = 1;
+        while (pursuitOn)
+        {
+            yield return new WaitForSeconds(1);
+            distanceSlider.value++;
+
+            if (distanceSlider.value >= currentWait)
+            {
+                pursuitOn = false;
+            }
+        }
+        Debug.Log("Caught up to cycle");
+        Instantiate(brokenCycle,brokenCycleSpawnPoint.position,Quaternion.identity);
+        yield return null;
     }
 }
