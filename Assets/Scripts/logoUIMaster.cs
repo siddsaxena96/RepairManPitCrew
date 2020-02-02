0e314d405ae9176087a2d00fd9c11f9e2442e8ab
sughoshpandey <sughoshpandey@gmail.com>
2020-02-02 20:38:15 +0800

    Connect user

--- Assets/Scripts/logoUIMaster.cs
+++ Assets/Scripts/logoUIMaster.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class logoUIMaster : MonoBehaviour
@@ -42,6 +43,7 @@ public class logoUIMaster : MonoBehaviour
             mainName.transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
             yield return new WaitForSeconds(0.3f);
         }
+        SceneManager.LoadScene("Game");
     }
 
     // Update is called once per frame
